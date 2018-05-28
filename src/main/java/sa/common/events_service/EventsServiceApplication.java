ccde98f333e2c2f7973b67f8b532f12f878e5a79
Bednarczyk <klby@gft.com>
2018-05-28 15:40:19 +0200

    setup repo

---
+++ src/main/java/sa/common/events_service/EventsServiceApplication.java
@@ -0,0 +1,12 @@
+package sa.common.events_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsServiceApplication.class, args);
+	}
+}
